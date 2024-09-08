3ca24b309e030eeef841009ba1bfc81c4b6c4faa
Taro Sato <okomestudio@gmail.com>
2024-09-08 15:29:19 -0700

    Remove reliance on with-eval-after-load

--- org-theme-ok.el
+++ org-theme-ok.el
@@ -30,21 +30,6 @@
 Use when contrast with non-outline contenst is desired."
   :group 'org-theme-ok)
 
-;;; Install fonts if missing
-(let* ((fonts '("URWClassico-Bold.otf"
-                "URWClassico-BoldItalic.otf"
-                "URWClassico-Italic.otf"
-                "URWClassico-Regular.otf"))
-       (url-prefix (file-name-concat "https://github.com/okomestudio/"
-                                     "fonts-urw-classico/raw/main/opentype/"))
-       (result (cl-remove-if #'null
-                             (mapcar (lambda (font)
-                                       (ok-font-install-from-url
-                                        (file-name-concat url-prefix font)))
-                                     fonts))))
-  (when result
-    (ok-font-cache-update)))
-
 (defvar oto-fixed-pitch-faces
   '(font-lock-builtin-face
     font-lock-comment-delimiter-face
@@ -81,8 +66,6 @@ Use when contrast with non-outline contenst is desired."
     org-verbatim)
   "Fixed-pitch faces in Org.")
 
-;;; FONT SCALING
-
 (defvar oto-outline-faces
   '((org-level-1 . '(:height 1.24))
     (org-level-2 . '(:height 1.12))
@@ -97,38 +80,105 @@ Use when contrast with non-outline contenst is desired."
 
 (push '("URW Classico" . 1.28) face-font-rescale-alist)
 
+;;; FONTS AND FACES
+
+;;; Install fonts if missing
+(let* ((fonts '("URWClassico-Bold.otf"
+                "URWClassico-BoldItalic.otf"
+                "URWClassico-Italic.otf"
+                "URWClassico-Regular.otf"))
+       (url-prefix (file-name-concat "https://github.com/okomestudio/"
+                                     "fonts-urw-classico/raw/main/opentype/"))
+       (result (cl-remove-if #'null
+                             (mapcar (lambda (font)
+                                       (ok-font-install-from-url
+                                        (file-name-concat url-prefix font)))
+                                     fonts))))
+  (when result
+    (ok-font-cache-update)))
+
+(let ((fontset "fontset-urw classic") frame)
+  ;; Create fontset that embeds the Japanese subset
+  (ok-fontset-create fontset
+                     oto-font-family-outline
+                     :subsets `((ja ,oto-font-family-outline-ja))
+                     :frame frame)
+  (set-face-attribute 'oto-face-outline frame :font fontset :fontset fontset)
+
+  ;; Apply outline face properties
+  (dolist (it oto-outline-faces)
+    (let* ((face (car it))
+           (prop (cdr it)))
+      (set-face-attribute
+       face frame
+       :height (or (cadr (assoc :height prop))
+                   (face-attribute face :height nil t))
+       :foreground (face-attribute face :foreground nil t)
+       :weight (face-attribute face :weight nil t)
+       :inherit 'oto-face-outline)))
+
+  ;; Make drawer a little less prominent
+  (set-face-attribute 'org-drawer frame
+                      :foreground (face-attribute 'shadow :foreground))
+
+  ;; Do not use bold face for links
+  (set-face-attribute 'org-link frame :weight 'normal))
+
+(defun oto--remap-to-mixed-pitch ()
+  (face-remap-add-relative 'default :inherit 'variable-pitch)
+  (dolist (face oto-fixed-pitch-faces)
+    (face-remap-add-relative face :inherit 'fixed-pitch)))
+
+(defun oto--handle-text-scale-mode ()
+  (let ((height (ok-face-text-scale-mode-height)))
+    (when height
+      (dolist (face (append oto-fixed-pitch-faces
+                            oto-outline-faces))
+        (if (consp face)
+            (setq face (car face)))
+        (face-remap-add-relative face :height height)))))
+
+(add-hook 'org-mode-hook #'oto--remap-to-mixed-pitch 91)
+(add-hook 'org-mode-hook #'oto--handle-text-scale-mode 92)
+;; (add-hook 'org-mode-hook #'foce-window-update 93)
+
+;; Do not use bold face for cite refs
+;; (set-face-attribute 'org-ref-cite-face nil :weight 'normal)
+
+(set-face-attribute 'org-modern-todo nil :weight 'bold)
+
 ;;; PER-MODE CONFIG
 
-(defun org-theme-ok--ad-apply-only-in-current-buffer-window (func &rest rest)
+(defun oto--ad-apply-only-in-current-buffer-window (func &rest rest)
   "Invoke FUNC only when current buffer window is visible."
   (when (get-buffer-window (current-buffer) t)
     (apply func rest)))
 
-(with-eval-after-load 'org-indent
-  (add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
-  (advice-add #'org-indent-refresh-maybe
-              :around #'org-theme-ok--ad-apply-only-in-current-buffer-window))
+;;; `org-indent'
+(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
+(advice-add #'org-indent-refresh-maybe
+            :around #'oto--ad-apply-only-in-current-buffer-window)
 
-(with-eval-after-load 'org-modern
-  (setopt org-modern-block-name t       ; use `org-modern-indent'
-          org-modern-checkbox '((?X . #("▢𐄂" 0 2 (composition ((2)))))
-                                (?- . #("▢–" 0 2 (composition ((2)))))
-                                (?\s . #("▢" 0 1 (composition ((1))))))
-          org-modern-hide-stars nil
-          org-modern-keyword "‣ "
-          org-modern-list '((?+ . "▷")
-                            (?- . "𑁋")  ; "–"
-                            (?* . "▶"))
-          org-modern-priority t
-          org-modern-replace-stars "◉🞛○▷"
-          org-modern-star 'replace
-          org-modern-statistics t
-          org-modern-table nil
-          org-modern-tag t
-          org-modern-timestamp t
-          org-modern-todo t)
-  (add-hook 'org-mode-hook #'org-modern-mode)
-  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
+;;; `org-modern'
+(setopt org-modern-block-name t  ; use `org-modern-indent'
+        org-modern-checkbox '((?X . #("▢𐄂" 0 2 (composition ((2)))))
+                              (?- . #("▢–" 0 2 (composition ((2)))))
+                              (?\s . #("▢" 0 1 (composition ((1))))))
+        org-modern-hide-stars nil
+        org-modern-keyword "‣ "
+        org-modern-list '((?+ . "▷")
+                          (?- . "𑁋")  ; "–"
+                          (?* . "▶"))
+        org-modern-priority t
+        org-modern-replace-stars "◉🞛○▷"
+        org-modern-star 'replace
+        org-modern-statistics t
+        org-modern-table nil
+        org-modern-tag t
+        org-modern-timestamp t
+        org-modern-todo t)
+(add-hook 'org-mode-hook #'org-modern-mode)
+(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
 ;;; `org-modern-indent'
 (defun org-modern-indent-ok--fix-top-level-indent ()
@@ -141,7 +191,7 @@ See github.com/jdtsmith/org-modern-indent/issues/10."
 (add-hook 'org-indent-mode-hook #'org-modern-indent-ok--fix-top-level-indent)
 (add-hook 'org-mode-hook #'org-modern-indent-mode 90)
 (advice-add #'org-modern-indent--refresh-watch
-            :around #'org-theme-ok--ad-apply-only-in-current-buffer-window)
+            :around #'oto--ad-apply-only-in-current-buffer-window)
 
 ;;; `valign'
 (setopt valign-fancy-bar t
@@ -156,60 +206,6 @@ See github.com/jdtsmith/org-modern-indent/issues/10."
     (valign-mode 1)))
 
 (add-hook 'org-mode-hook #'valign-ok--maybe-activate)
-
-;;; Fonts and faces
-(with-eval-after-load 'org-faces
-  (let ((fontset "fontset-urw classic") frame)
-    ;; Create fontset that embeds the Japanese subset
-    (ok-fontset-create fontset
-                       oto-font-family-outline
-                       :subsets `((ja ,oto-font-family-outline-ja))
-                       :frame frame)
-    (set-face-attribute 'oto-face-outline frame :font fontset :fontset fontset)
-
-    ;; Apply outline face properties
-    (dolist (it oto-outline-faces)
-      (let* ((face (car it))
-             (prop (cdr it)))
-        (set-face-attribute
-         face frame
-         :height (or (cadr (assoc :height prop))
-                     (face-attribute face :height nil t))
-         :foreground (face-attribute face :foreground nil t)
-         :weight (face-attribute face :weight nil t)
-         :inherit 'oto-face-outline)))
-
-    ;; Make drawer a little less prominent
-    (set-face-attribute 'org-drawer frame
-                        :foreground (face-attribute 'shadow :foreground))
-
-    ;; Do not use bold face for links
-    (set-face-attribute 'org-link frame :weight 'normal))
-
-  (defun oto--remap-to-mixed-pitch ()
-    (face-remap-add-relative 'default :inherit 'variable-pitch)
-    (dolist (face oto-fixed-pitch-faces)
-      (face-remap-add-relative face :inherit 'fixed-pitch)))
-
-  (defun oto--handle-text-scale-mode ()
-    (let ((height (ok-face-text-scale-mode-height)))
-      (when height
-        (dolist (face (append oto-fixed-pitch-faces
-                              oto-outline-faces))
-          (if (consp face)
-              (setq face (car face)))
-          (face-remap-add-relative face :height height)))))
-
-  (add-hook 'org-mode-hook #'oto--remap-to-mixed-pitch 91)
-  (add-hook 'org-mode-hook #'oto--handle-text-scale-mode 92)
-  ;; (add-hook 'org-mode-hook #'foce-window-update 93)
-  )
-
-(with-eval-after-load 'org-ref
-  ;; Do not use bold face for cite refs
-  (set-face-attribute 'org-ref-cite-face nil :weight 'normal))
-
-(set-face-attribute 'org-modern-todo nil :weight 'bold)
 
 (provide 'org-theme-ok)
 
