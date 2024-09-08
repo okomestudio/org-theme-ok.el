42ede081994d6dcfeb9deb7fa7df0552cd773697
Taro Sato <okomestudio@gmail.com>
2024-09-07 18:05:49 -0700

    Clean up after refactoring

--- org-theme-ok.el
+++ org-theme-ok.el
@@ -1,21 +1,40 @@
 ;;; org-theme-ok.el --- Org Theme for Okome Studio  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; Currently, only the light theme with minor adjustments for use with
+;; `flexoki-themes' is implemented.
+;;
 ;;; Code:
 
 (require 'ok)
 
-(defvar org-theme-ok-font-family-outline "URW Classico"
+(defvar oto-font-family-outline "URW Classico"
   "Font for outlines.")
 
-(defvar org-theme-ok-font-family-outline-ja "Noto Sans CJK JP"
+(defvar oto-font-family-outline-ja "Noto Sans CJK JP"
   "Font for outlines in Japanese.")
 
-(defface org-theme-ok-face-outline '((t :inherit 'default))
+(defface oto-face-outline '((t :inherit 'default))
   "Face for outlines.
 Use when contrast with non-outline contenst is desired."
   :group 'org-theme-ok)
 
-(defvar org-theme-ok-fixed-pitch-faces
+;;; Install fonts if missing
+(let* ((fonts '("URWClassico-Bold.otf"
+                "URWClassico-BoldItalic.otf"
+                "URWClassico-Italic.otf"
+                "URWClassico-Regular.otf"))
+       (url-prefix (file-name-concat "https://github.com/okomestudio/"
+                                     "fonts-urw-classico/raw/main/opentype/"))
+       (result (cl-remove-if #'null
+                             (mapcar (lambda (font)
+                                       (ok-font-install-from-url
+                                        (file-name-concat url-prefix font)))
+                                     fonts))))
+  (when result
+    (ok-font-cache-update)))
+
+(defvar oto-fixed-pitch-faces
   '(font-lock-builtin-face
     font-lock-comment-delimiter-face
     font-lock-comment-face
@@ -49,9 +68,9 @@ Use when contrast with non-outline contenst is desired."
     org-special-keyword
     ;; org-table
     org-verbatim)
-  "Fixed-pitch faces in Org mode.")
+  "Fixed-pitch faces in Org.")
 
-(defvar org-theme-ok-outline-faces
+(defvar oto-outline-faces
   '((org-level-1 . '(:height 1.24))
     (org-level-2 . '(:height 1.12))
     (org-level-3 . '(:height 1.00))
@@ -61,7 +80,7 @@ Use when contrast with non-outline contenst is desired."
     (org-level-7 . '(:height 0.90))
     (org-level-8 . '(:height 0.90))
     (org-document-title . '(:height 1.24)))
-  "Base outlines faces used in Org mode.")
+  "Base outlines faces used in Org.")
 
 (push '("URW Classico" . 1.28) face-font-rescale-alist)
 
@@ -71,7 +90,7 @@ Use when contrast with non-outline contenst is desired."
          (background (face-attribute face :background nil t))
          (foreground (face-attribute face :foreground nil t))
          (weight (face-attribute face :weight nil t))
-         (inherit 'org-theme-ok-face-outline))
+         (inherit 'oto-face-outline))
     (set-face-attribute face nil
                         :height height
                         :foreground "white"
@@ -80,32 +99,24 @@ Use when contrast with non-outline contenst is desired."
                         :inherit inherit)))
 
 (with-eval-after-load 'org-faces
-  ;;   :ensure-system-package
-  ;;   ("~/.local/share/fonts/u/URWClassico-Regular.otf"
-  ;;    . "~/.config/emacs/bin/install-font \
-  ;; 'https://github.com/okomestudio/fonts-urw-classico/raw/main/opentype/URWClassico-Bold.otf' \
-  ;; 'https://github.com/okomestudio/fonts-urw-classico/raw/main/opentype/URWClassico-BoldItalic.otf' \
-  ;; 'https://github.com/okomestudio/fonts-urw-classico/raw/main/opentype/URWClassico-Italic.otf' \
-  ;; 'https://github.com/okomestudio/fonts-urw-classico/raw/main/opentype/URWClassico-Regular.otf'")
-
   (let ((fontset "fontset-urw classic")
         frame)
     ;; Create fontset.
-    (ok-face-fontset-create fontset
-                            org-theme-ok-font-family-outline
-                            :subsets `((ja ,org-theme-ok-font-family-outline-ja))
-                            :frame frame)
-    (set-face-attribute 'org-theme-ok-face-outline frame :font fontset :fontset fontset)
+    (ok-fontset-create fontset
+                       oto-font-family-outline
+                       :subsets `((ja ,oto-font-family-outline-ja))
+                       :frame frame)
+    (set-face-attribute 'oto-face-outline frame :font fontset :fontset fontset)
 
     ;; Set faces.
-    (dolist (it org-theme-ok-outline-faces)
+    (dolist (it oto-outline-faces)
       (let* ((face (car it))
              (prop (cdr it))
              (height (or (car (cdr (assoc :height prop)))
                          (face-attribute face :height nil t)))
              (foreground (face-attribute face :foreground nil t))
              (weight (face-attribute face :weight nil t))
-             (inherit 'org-theme-ok-face-outline))
+             (inherit 'oto-face-outline))
         (set-face-attribute face frame
                             :height height
                             :foreground foreground
@@ -120,22 +131,22 @@ Use when contrast with non-outline contenst is desired."
     (with-eval-after-load 'org-ref
       (set-face-attribute 'org-ref-cite-face nil :weight 'normal)))
 
-  (defun org-theme-ok--remap-to-mixed-pitch ()
+  (defun oto--remap-to-mixed-pitch ()
     (face-remap-add-relative 'default :inherit 'variable-pitch)
-    (dolist (face org-theme-ok-fixed-pitch-faces)
+    (dolist (face oto-fixed-pitch-faces)
       (face-remap-add-relative face :inherit 'fixed-pitch)))
 
-  (defun org-theme-ok--handle-text-scale-mode ()
+  (defun oto--handle-text-scale-mode ()
     (let ((height (ok-face-text-scale-mode-height)))
       (when height
-        (dolist (face (append org-theme-ok-fixed-pitch-faces
-                              org-theme-ok-outline-faces))
+        (dolist (face (append oto-fixed-pitch-faces
+                              oto-outline-faces))
           (if (consp face)
               (setq face (car face)))
           (face-remap-add-relative face :height height)))))
 
-  (add-hook 'org-mode-hook #'org-theme-ok--remap-to-mixed-pitch 91)
-  (add-hook 'org-mode-hook #'org-theme-ok--handle-text-scale-mode 92)
+  (add-hook 'org-mode-hook #'oto--remap-to-mixed-pitch 91)
+  (add-hook 'org-mode-hook #'oto--handle-text-scale-mode 92)
   ;; (add-hook 'org-mode-hook #'foce-window-update 93)
   )
 
