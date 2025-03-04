4b0045a0792781c95f061a544e851fdb1dbde2de
Taro Sato <okomestudio@gmail.com>
2025-03-04 01:03:46 -0800

    Refactor to make it a minor-mode

--- org-theme-ok.el
+++ org-theme-ok.el
@@ -1,7 +1,26 @@
 ;;; org-theme-ok.el --- Org Theme for Okome Studio  -*- lexical-binding: t -*-
 ;;
-;; Version: 0.1
-;; Package-Requires: ((emacs "29.1") (org "9.7") (org-modern "1.5") (valign "3.1.1"))
+;; Copyright (C) 2024-2025 Taro Sato
+;;
+;; Author: Taro Sato <okomestudio@gmail.com>
+;; URL: https://github.com/okomestudio/org-theme-ok.el
+;; Version: 0.2.1
+;; Package-Requires: ((emacs "29.1") (ok "0.2.1") (org "9.7") (org-modern "1.5") (org-modern-indent "0.1.4") (valign "3.1.1"))
+;;
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
@@ -17,6 +36,7 @@
 (require 'ok)
 (require 'org)
 (require 'org-modern)
+(require 'org-modern-indent)
 (require 'valign)
 
 (defvar oto-font-family-outline "URW Classico"
@@ -78,11 +98,19 @@ Use when contrast with non-outline contenst is desired."
     (org-document-title . '(:height 1.6)))
   "Base outlines faces used in Org.")
 
-(push '("URW Classico" . 1.28) face-font-rescale-alist)
+;;; FONT RESCALE
+
+(defun oto--fonts-rescale-add ()
+  "Add fonts to rescale alist."
+  (push '("URW Classico" . 1.28) face-font-rescale-alist))
+
+(defun oto--fonts-rescale-remove ()
+  "Remove fonts from rescale alist."
+  (push '("URW Classico" . nil) face-font-rescale-alist))
 
 ;;; FONTS AND FACES
 
-;;; Install fonts if missing
+;; Install missing fonts.
 (let* ((fonts '("URWClassico-Bold.otf"
                 "URWClassico-BoldItalic.otf"
                 "URWClassico-Italic.otf"
@@ -106,30 +134,47 @@ Use when contrast with non-outline contenst is desired."
   (set-face-attribute 'oto-face-outline frame
                       :family oto-font-family-outline
                       ;; :font fontset
-                      :fontset fontset)
+                      :fontset fontset))
 
-  ;; Apply outline face properties
-  (dolist (it oto-outline-faces)
-    (let ((face (car it))
-          (prop (cdr it)))
-      (set-face-attribute face frame
-                          :height (or (cadr (assoc :height prop))
-                                      (face-attribute face :height nil t))
-                          :foreground (face-attribute face :foreground nil t)
-                          :weight (face-attribute face :weight nil t)
-                          :inherit 'oto-face-outline)))
+;; Apply outline face properties. This enables different faces (e.g.,
+;; serif vs. sans serif) for the body text and the outline text.
+(dolist (it oto-outline-faces)
+  (let ((face (car it))
+        (prop (cdr it)))
+    (set-face-attribute face nil
+                        :height (or (cadr (assoc :height prop))
+                                    (face-attribute face :height nil t))
+                        :foreground (face-attribute face :foreground nil t)
+                        :weight (face-attribute face :weight nil t)
+                        :inherit 'oto-face-outline)))
 
-  ;; Make drawer a little less prominent
-  (set-face-attribute 'org-drawer frame
+(defun oto--adjust-faces (theme)
+  "Adjust font and faces for the new THEME.
+The properties of the faces defined by less popular packages aren't
+properly adjusted on theme change due to lack of handler code. Do it
+here."
+  (set-face-attribute 'org-hide nil
+                      :foreground (face-attribute 'default :background)
+                      :background (face-attribute 'default :background))
+
+  ;; Make drawer a little less prominent.
+  (set-face-attribute 'org-drawer nil
                       :foreground (face-attribute 'shadow :foreground))
 
-  ;; Do not use bold face for links
-  (set-face-attribute 'link frame :weight 'unspecified))
+  ;; Do not use bold face for links.
+  (set-face-attribute 'link nil :weight 'unspecified)
 
-;; Do not use bold face for cite refs
-;; (set-face-attribute 'org-ref-cite-face nil :weight 'normal)
+  ;; Do not use bold face for cite refs.
+  ;; (set-face-attribute 'org-ref-cite-face nil :weight 'normal)
 
-(set-face-attribute 'org-modern-todo nil :weight 'bold)
+  ;; Make TODO face prominent.
+  (set-face-attribute 'org-modern-todo nil :weight 'bold)
+
+  ;; Ensure tags are visible.
+  (set-face-attribute 'org-modern-tag nil
+                      :foreground (face-attribute 'default :foreground)))
+
+(add-hook 'enable-theme-functions #'oto--adjust-faces)
 
 ;;; PER-MODE CONFIG
 
@@ -147,24 +192,26 @@ Use when contrast with non-outline contenst is desired."
             (setq face (car face)))
         (face-remap-add-relative face :height height)))))
 
-(defun oto--ad-apply-only-in-current-buffer-window (func &rest rest)
-  "Invoke FUNC only when current buffer window is visible."
+(defun oto--run-only-when-visible (fun &rest _)
+  "Run FUN only if current buffer window is visible.
+This is an advice to reduce unnecessary rendering."
   (when (get-buffer-window (current-buffer) t)
-    (apply func rest)))
+    (apply fun _)))
 
 ;;; `org-indent'
-(advice-add #'org-indent-refresh-maybe
-            :around #'oto--ad-apply-only-in-current-buffer-window)
+
+(advice-add #'org-indent-refresh-maybe :around #'oto--run-only-when-visible)
 
 ;;; `org-modern'
-(setopt org-modern-block-name t  ; use `org-modern-indent'
+
+(setopt org-modern-block-name t         ; use `org-modern-indent'
         org-modern-checkbox '((?X . #("▢𐄂" 0 2 (composition ((2)))))
                               (?- . #("▢–" 0 2 (composition ((2)))))
                               (?\s . #("▢" 0 1 (composition ((1))))))
         org-modern-hide-stars nil
         org-modern-keyword "‣ "
         org-modern-list '((?+ . "▷")
-                          (?- . "𑁋")  ; "–"
+                          (?- . "𑁋")    ; "–"
                           (?* . "▶"))
         org-modern-priority t
         org-modern-replace-stars "◉🞛○▷"
@@ -174,9 +221,9 @@ Use when contrast with non-outline contenst is desired."
         org-modern-tag t
         org-modern-timestamp t
         org-modern-todo t)
-(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
 ;;; `org-modern-indent'
+
 (defun org-modern-indent-ok--fix-top-level-indent ()
   "Activate org-modern-indent on the top-level file node.
 See github.com/jdtsmith/org-modern-indent/issues/10."
@@ -185,10 +232,11 @@ See github.com/jdtsmith/org-modern-indent/issues/10."
             0 (propertize " " 'face 'org-indent))))
 
 (add-hook 'org-indent-mode-hook #'org-modern-indent-ok--fix-top-level-indent)
-(advice-add #'org-modern-indent--refresh-watch
-            :around #'oto--ad-apply-only-in-current-buffer-window)
+(advice-add #'org-modern-indent--refresh-watch :around
+            #'oto--run-only-when-visible)
 
 ;;; `valign'
+
 (setopt valign-fancy-bar t
         valign-max-table-size 4000
         valign-signal-parse-error t)
@@ -197,20 +245,40 @@ See github.com/jdtsmith/org-modern-indent/issues/10."
   "Default max-buffer-size above which `valign-mode' will not activate.")
 
 (defun valign-ok--maybe-activate ()
+  "Activate `valign' only if buffer is small enough."
   (when (<= (buffer-size) valign-ok--max-buffer-size)
     (valign-mode 1)))
 
-(defun oto-apply ()
-  "Apply `org-theme-ok' theme in Org."
+;;; MODE HOOK
+
+(defun oto--activate ()
+  "Activate `org-theme-ok-mode'.
+When creating a minor mode, this would be the mode activator function."
   (org-indent-mode 1)
   (org-modern-mode 1)
   (org-modern-indent-mode 1)
   (oto--remap-to-mixed-pitch)
   ;; (oto--handle-text-scale-mode)
-  ;; (foce-window-update)
-  (valign-ok--maybe-activate))
+  (valign-ok--maybe-activate)
+  (oto--fonts-rescale-add)
+  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
 
-(add-hook 'org-mode-hook #'oto-apply 90)
+(defun oto--deactivate ()
+  "Deactivate `org-theme-ok-mode'."
+  (remove-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  (oto--fonts-rescale-remove)
+  (valign-mode -1)
+  (org-modern-indent-mode -1)
+  (org-modern-mode -1)
+  (org-indent-mode -1))
+
+;;;###autoload
+(define-minor-mode org-theme-ok-mode
+  "A minor mode for Okome Studio Org theme."
+  :group 'org
+  :lighter "org-them-ok-mode"
+  :keymap nil
+  (if org-theme-ok-mode (oto--activate) (oto--deactivate)))
 
 (provide 'org-theme-ok)
 
